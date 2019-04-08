1b81c7aed7cc247fd400c781fcd5c4f0cb90e3e8
Sergey Ivanov <plastilin1998@gmail.com>
2019-04-08 20:40:51 +0800

    -

--- ObjJKH/Program/MainMenu/MainMenu.swift
+++ ObjJKH/Program/MainMenu/MainMenu.swift
@@ -702,9 +702,9 @@ class MainMenu: UIViewController, UITableViewDelegate, UITableViewDataSource {
                                                                             sum = String(format:"%.2f", json_bills["Sum"] as! Double)
 //                                                                            let s = json_bills["Sum"] as! Double
                                                                             sumFine = String(format:"%.2f", json_bills["SumFine"] as! Double)
-                                                                            sum = "0.00"
-                                                                            let s = 0
-                                                                            sumFine = "0.00"
+//                                                                            sum = "0.00"
+//                                                                            let s = 0
+//                                                                            sumFine = "0.00"
                                                                             self.debtIdent.append(ls)
                                                                             self.debtSum.append(sum)
                                                                             self.debtSumFine.append(sumFine)
