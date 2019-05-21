bdfa0db64c7f1e038fb6f615fa595c7b4133ff6a
Sergey Ivanov <plastilin1998@gmail.com>
2019-05-21 13:59:57 +0800

    -

--- ObjJKH/Program/MainMenu/Pays/PaysController.swift
+++ ObjJKH/Program/MainMenu/Pays/PaysController.swift
@@ -114,7 +114,17 @@ class PaysController: UIViewController, DropperDelegate, UITableViewDelegate, UI
             let cancelAction = UIAlertAction(title: "Сохранить", style: .default) { (_) -> Void in
                 let textField = alert.textFields![0]
                 let str = textField.text
-                if ((str?.contains("@"))! && (str?.contains(".ru"))!) || ((str?.contains("@"))! && (str?.contains(".com"))!){
+                var kD = 0
+                var kS = 0
+                str!.forEach{
+                    if $0 == "."{
+                        kD += 1
+                    }
+                    if $0 == "@"{
+                        kS += 1
+                    }
+                }
+                if ((str?.contains("@"))!) && ((str?.contains("."))!) && kD == 1 && kS == 1{
                     UserDefaults.standard.set(str, forKey: "mail")
                     self.payed()
                 }else{
