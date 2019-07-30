1a21fb1c2c86ca3c34319236171b15e81e0183b7
RomanXXI <tra-ss@mail.ru>
2019-07-30 20:17:01 +0700

    - новый релиз Сервис и Комфорт 2.23

--- ObjJKH/Program/MainMenu/Pays/PaysMytishiController.swift
+++ ObjJKH/Program/MainMenu/Pays/PaysMytishiController.swift
@@ -164,7 +164,8 @@ class PaysMytishiController: UIViewController, DropperDelegate, UITableViewDeleg
 //        #if isUKKomfort
 //        self.payedS()
 //        #elseif isStolitsa
-        #if isKlimovsk12 || isMupRCMytishi || isUpravdomChe || isReutKomfort || isServiceKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab
+//        #if isKlimovsk12 || isMupRCMytishi || isUpravdomChe || isReutKomfort || isServiceKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab
+        #if isKlimovsk12 || isMupRCMytishi || isUpravdomChe || isReutKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab
         l = true
         #else
         self.payedS()
@@ -253,9 +254,9 @@ class PaysMytishiController: UIViewController, DropperDelegate, UITableViewDeleg
         #elseif isReutKomfort
         shopCode = "234821"
         targetName = "УК РеутКомфорт"
-        #elseif isServiceKomfort
-        shopCode = "252187"
-        targetName = "УК Сервис и Комфорт"
+//        #elseif isServiceKomfort
+//        shopCode = "252187"
+//        targetName = "УК Сервис и Комфорт"
         #elseif isServicekom
         shopCode = "254158"
         targetName = "Сервиском"
@@ -426,7 +427,8 @@ class PaysMytishiController: UIViewController, DropperDelegate, UITableViewDeleg
                     self.present(alert, animated: true, completion: nil)
                 }
             }
-            #elseif isKlimovsk12 || isUpravdomChe || isReutKomfort || isServiceKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab
+//            #elseif isKlimovsk12 || isUpravdomChe || isReutKomfort || isServiceKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab
+            #elseif isKlimovsk12 || isUpravdomChe || isReutKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab
             if selectLS == "Все"{
                 let str_ls = UserDefaults.standard.string(forKey: "str_ls")!
                 let str_ls_arr = str_ls.components(separatedBy: ",")
