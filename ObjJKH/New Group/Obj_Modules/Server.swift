1b307a5350b822b2b33316caf469c0648245fe88
RomanXXI <tra-ss@mail.ru>
2019-08-09 07:54:24 +0700

    - новый таргет Кристалл

--- ObjJKH/New Group/Obj_Modules/Server.swift
+++ ObjJKH/New Group/Obj_Modules/Server.swift
@@ -87,6 +87,8 @@ class Server {
         static let SERVER          = "http://uk-gkh.org/tsn_dnestrovskaya12/"
     #elseif isTSN_Ruble40
         static let SERVER          = "http://uk-gkh.org/romacshkovo/"
+    #elseif isCristall
+        static let SERVER          = "http://uk-gkh.org/ooo_kristal/"
     #endif
     
     static let SEND_SUPPORT        = "SendEmailMessage.ashx?"            // Получение настроек для приложения
