3b0575bab0aa439aedc8eb09c6c92b10bbc18f41
Sergey Ivanov <plastilin1998@gmail.com>
2019-11-25 16:07:25 +0800

    - Лого притомское

---
+++ VodSergPosad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VodSergPosad
+//
+//  Created by Sergey Ivanov on 25/11/2019.
+//  Copyright © 2019 The Best. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
