c6564cdaeeab523028ff53304d75e5f4d14bd790
Roman <tr@sm-center.ru>
2019-12-20 11:08:33 +0300

    -

---
+++ Pods/Target Support Files/Pods-GorAntenService/Pods-GorAntenService-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GorAntenService : NSObject
+@end
+@implementation PodsDummy_Pods_GorAntenService
+@end
---
+++ Pods/Target Support Files/Pods-Zarinsk/Pods-Zarinsk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Zarinsk : NSObject
+@end
+@implementation PodsDummy_Pods_Zarinsk
+@end
