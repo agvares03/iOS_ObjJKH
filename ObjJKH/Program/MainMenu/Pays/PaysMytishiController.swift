90f7925be822b143acb438e5841c0fb3b77d6a18
Sergey Ivanov <plastilin1998@gmail.com>
2020-06-30 18:49:20 +0800

    Тинькофф Суханова Парк

--- ObjJKH/Program/MainMenu/Pays/PaysMytishiController.swift
+++ ObjJKH/Program/MainMenu/Pays/PaysMytishiController.swift
@@ -178,7 +178,7 @@ class PaysMytishiController: UIViewController, DropperDelegate, UITableViewDeleg
     }
     @IBAction func Payed(_ sender: UIButton) {
         var l = false
-        #if isKlimovsk12 || isVestSnab || isTSJ_Life || isSouthValley || isGumse || isSV14 || isGrinvay || isTafgai || isSibir || isRKC_Samara || isJilUpravKom || isClean_Tid || isEJF || isMaximum || isVodSergPosad || isSuhanovo || isDoka || isPerspectiva || isInvest || isUniversSol || isClearCity || isZarinsk || isDOM24 || isPedagog || isMobileMIR || isMonolit || isRIC || isStolitsa || isMupRCMytishi || isUpravdomChe || isReutKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab || isAFregat || isRodnikMUP || isElectroSbitSaratov || isJKH_Pavlovskoe || isNewOpaliha || isStroiDom || isDJVladimir || isSibAliance || isTSJ_Rachangel || isMUP_IRKC || isNarianMarEl || isParitet || isTSN_Ruble40 || isEnergoProgress
+        #if isKlimovsk12 || isSuhanovo || isVestSnab || isTSJ_Life || isSouthValley || isGumse || isSV14 || isGrinvay || isTafgai || isSibir || isRKC_Samara || isJilUpravKom || isClean_Tid || isEJF || isMaximum || isVodSergPosad || isSuhanovo || isDoka || isPerspectiva || isInvest || isUniversSol || isClearCity || isZarinsk || isDOM24 || isPedagog || isMobileMIR || isMonolit || isRIC || isStolitsa || isMupRCMytishi || isUpravdomChe || isReutKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab || isAFregat || isRodnikMUP || isElectroSbitSaratov || isJKH_Pavlovskoe || isNewOpaliha || isStroiDom || isDJVladimir || isSibAliance || isTSJ_Rachangel || isMUP_IRKC || isNarianMarEl || isParitet || isTSN_Ruble40 || isEnergoProgress
         l = true
         #else
         self.payedS()
@@ -676,6 +676,9 @@ class PaysMytishiController: UIViewController, DropperDelegate, UITableViewDeleg
         #elseif isVestSnab
         shopCode = "355954"
         targetName = "УК Вест-Снаб"
+        #elseif isSuhanovo
+        shopCode = "356524"
+        targetName = "УК Суханово Парк"
         #endif
         self.totalSum = Double(k.replacingOccurrences(of: " руб.", with: ""))!
         self.sum = Double(l.replacingOccurrences(of: " руб.", with: ""))!
@@ -809,7 +812,8 @@ class PaysMytishiController: UIViewController, DropperDelegate, UITableViewDeleg
             let amount = NSNumber(floatLiteral: self.totalSum)
             defaults.set(defaults.string(forKey: "login"), forKey: "CustomerKey")
             defaults.synchronize()
-            print(receiptData)
+//            print(Data)
+//            print(receiptData)
 //            if payType == 1{
 //                let address = PKContact()
 //                address.emailAddress = defaults.object(forKey: "mail")! as? String
@@ -837,7 +841,7 @@ class PaysMytishiController: UIViewController, DropperDelegate, UITableViewDeleg
                 }
 //            }
             //            #elseif isKlimovsk12 || isUpravdomChe || isReutKomfort || isServiceKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab
-            #elseif isKlimovsk12 || isVestSnab || isTSJ_Life || isSouthValley || isGumse || isSV14 || isGrinvay || isTafgai || isSibir || isRKC_Samara || isJilUpravKom || isClean_Tid || isEJF || isMaximum || isVodSergPosad || isSuhanovo || isDoka || isPerspectiva || isInvest || isUniversSol || isClearCity || isZarinsk || isPedagog || isMobileMIR || isEasyLife || isMonolit || isRIC || isEnergoProgress || isStolitsa || isUpravdomChe || isReutKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab || isAFregat || isRodnikMUP || isElectroSbitSaratov || isJKH_Pavlovskoe || isNewOpaliha || isStroiDom || isDJVladimir || isSibAliance || isTSJ_Rachangel || isMUP_IRKC || isNarianMarEl || isParitet || isTSN_Ruble40
+            #elseif isKlimovsk12 || isSuhanovo || isVestSnab || isTSJ_Life || isSouthValley || isGumse || isSV14 || isGrinvay || isTafgai || isSibir || isRKC_Samara || isJilUpravKom || isClean_Tid || isEJF || isMaximum || isVodSergPosad || isSuhanovo || isDoka || isPerspectiva || isInvest || isUniversSol || isClearCity || isZarinsk || isPedagog || isMobileMIR || isEasyLife || isMonolit || isRIC || isEnergoProgress || isStolitsa || isUpravdomChe || isReutKomfort || isServicekom || isUKGarant || isParus || isTeplovodoresources || isStroimBud || isRodnikMUP || isUKParitetKhab || isAFregat || isRodnikMUP || isElectroSbitSaratov || isJKH_Pavlovskoe || isNewOpaliha || isStroiDom || isDJVladimir || isSibAliance || isTSJ_Rachangel || isMUP_IRKC || isNarianMarEl || isParitet || isTSN_Ruble40
             if selectLS == "Все"{
                 let str_ls = UserDefaults.standard.string(forKey: "str_ls")!
                 let str_ls_arr = str_ls.components(separatedBy: ",")
